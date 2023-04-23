bedea7ebb6ce10291c15c106ef049e194bb36752
jittu <onekb.twotb@gmail.com>
2023-04-23 12:31:55 +0530

    adding like model

---
+++ src/main/java/com/blogapp/entities/SalesModule.java
@@ -0,0 +1,16 @@
+package com.blogapp.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class SalesModule {
+    String name;
+    String department;
+
+}
